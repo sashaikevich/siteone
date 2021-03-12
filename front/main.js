4e95d943129f85dc4d004827812ca2ccc27802cd
Sasha <me@sashaikevich.com>
2021-03-12 10:55:54 +0100

    added logo, for a basic LP

--- front/main.js
+++ front/main.js
@@ -1,4 +1,6 @@
-console.log('it works');
+import './style/main.scss'
+
+console.log('it worsks');
 
 if (module.hot) {
   module.hot.accept();
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,21 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundled.js'
   },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
