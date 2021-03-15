71d698426739ccae6c08485379960b7679203354
Sasha <me@sashaikevich.com>
2021-03-15 15:40:31 +0100

    animations adde

--- front/main.js
+++ front/main.js
@@ -1,6 +1,12 @@
 import './style/main.scss'
 
-console.log('it worsks');
+window.addEventListener('load', ()=>{
+  document.body.classList.add('loaded');
+  // setTimeout(()=>{
+  //   // reveal image background 
+  //   document.querySelector('.img-bg-wrapper').classList.add('loaded')
+  // },2000)
+})
 
 if (module.hot) {
   module.hot.accept();
