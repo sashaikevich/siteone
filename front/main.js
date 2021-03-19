f18357b03019fffb22039ee629f69ab0bcd6a1c0
Sasha <me@sashaikevich.com>
2021-03-19 01:03:12 +0100

    added some redirection logic

--- front/main.js
+++ front/main.js
@@ -1,11 +1,34 @@
 import './style/main.scss'
 
-window.addEventListener('load', ()=>{
+window.addEventListener('load', () => {
   document.body.classList.add('loaded');
-  // setTimeout(()=>{
-  //   // reveal image background 
-  //   document.querySelector('.img-bg-wrapper').classList.add('loaded')
-  // },2000)
+
+  const redWrap = document.querySelector('.redirection-wrapper');
+
+  let redirectTimeout;
+  const cancelEl = document.createElement('a');
+  cancelEl.innerText = "cancel";
+  cancelEl.setAttribute("href", "#");
+  cancelEl.addEventListener('click', cancelRedirection);
+
+
+  setTimeout(() => {
+    // rediret to insta
+    redWrap.classList.add('visible');
+    redWrap.appendChild(cancelEl);
+    redirectTimeout = setTimeout(() => {
+      // window.location.href = "http://www.instagram.com";
+      console.log('redirect');
+    }, 6000) // equals animation duration (6s)
+  }, 3000) // equals animation delay
+
+  function cancelRedirection(e) {
+    e.preventDefault();
+    document.querySelector('.auto-redirect').classList.remove('visible')
+    document.querySelector('.manual-redirect').classList.add('visible')
+    clearTimeout(redirectTimeout);
+  }
+
 })
 
 if (module.hot) {
